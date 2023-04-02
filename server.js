a062ada6596942f7fff6cfcc097fb89b481f9fe4
Domenik Kranke <30910026+dkranke@users.noreply.github.com>
2023-04-02 21:36:06 +0200

    Docker configuration added

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var port = process.env.PORT || 8080;
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var requireHeader = parseEnvList(process.env.CORSANYWHERE_REQUIREHEADER);
+var removeHeaders = parseEnvList(process.env.CORSANYWHERE_REMOVEHEADERS);
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -23,27 +25,10 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: requireHeader,
   checkRateLimit: checkRateLimit,
-  removeHeaders: [
-    'cookie',
-    'cookie2',
-    // Strip Heroku-specific headers
-    'x-request-start',
-    'x-request-id',
-    'via',
-    'connect-time',
-    'total-route-time',
-    // Other Heroku added debug headers
-    // 'x-forwarded-for',
-    // 'x-forwarded-proto',
-    // 'x-forwarded-port',
-  ],
-  redirectSameOrigin: true,
-  httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-    xfwd: false,
-  },
+  removeHeaders: removeHeaders,
+  redirectSameOrigin: true
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
